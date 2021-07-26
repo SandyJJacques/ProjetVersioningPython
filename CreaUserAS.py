927ab7663cc5e9ce4446266eade2f1fa254b6df8
SandyJJacques <82026671+SandyJJacques@users.noreply.github.com>
2021-07-26 18:27:17 +0200

    New version finale crea user

--- CreaUserAS.py
+++ CreaUserAS.py
@@ -1,47 +1,36 @@
-import csv
-import random
-import string
-import hashlib
+ef create_user(ville):
+    create=y
 
-##Création de la condition de lancement de création de user
-create='Y'                                                                      
+    while create =="y" or create == "Y":
+        id1 = input("id : ")
+        nom = input("Nom : ")
+        prenom = input("Prénom :")
+        tel = input("N° de téléphone : ")
 
-##Verification de condition de creation de user
-while create == 'Y' or create == 'y':
+        mail = (prenom+"."+nom+"@netway.fr")
+        mail = (mail.lower())
+        print("Mail : ",mail)
+        pwd = password_generator()
 
-    ##Attribution du csv en fonction du site saisi par l'admin
-    site=input("Site de l'utilisateur [GRE] [REN] [STR]: ")
-    if site != 'GRE' or site != 'REN' or site != 'STR':
-        site=input('Veuillez saisir un site correct [GRE, REN ou STR] : ')
-    siteuser=('User'+site+'.csv')                                               
+        print("Mot de passe : ", pwd)
+        mdp = hashlib.sha256(pwd.encode()).hexdigest()
+        
 
-    ##Saisie des informations du user  
-    Nom=input("NOM de l'utilisateur : ")                                            
-    Prenom=input("Prénom de l'utilisateur : ")
-    Tel=input("N° de telephone de l'utilisateur :")
-    longueur=len(Tel)
-    if longueur != 10:
-        Tel=input("Le numéro de téléphone n'est pas valide. Merci de la saisir à nouveau : ")
+        db = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='Projet2020!',
+            database='python'
+        )
+        mycursor = db.cursor()
+        sql = ("INSERT INTO %s(id, Nom, Prénom, Tel, Mail, Mot de passe)VALUES(%s, %s, %s, %s, %s) ", ville, id1, nom, prenom, tel, mail, mdp)
+        mycursor.execute(sql)
+        db.commit()
+        return("L'utilisateur a été créé")
 
-    ##Creation du mail en fonction du nom et prenom
-    Mail=(Prenom+'.'+Nom+'@netway.fr')
-    Mail=(Mail.lower())
-    print(Mail)
-
-    ##Génération du pwd                        
-    mot_de_passe=string.ascii_letters+string.digits
-    ##Initialisation du pwd aléatoire
-    passwd=""
-    for i in range(12):         ##pwd de taille 12
-        passwd+=mot_de_passe[random.randint(0,len(mot_de_passe)-1)]
-    salt='Netway'
-
-    ##Ajout du nouveau user au fichier csv de son site
-    with open(siteuser,'a',newline='', encoding='utf-8') as fichiercsv:
-        writer=csv.writer(fichiercsv, delimiter=';')
-        writer.writerow([Nom, Prenom, Mail, Tel, passwd])
-
-    ##Demande si ajout d'un nouveau user ou non
-    create=input("Voulez-vous créer un autre utilisateur ? [Y] [N] ")
-
-fichiercsv.close() ##Fermeture du fichier csv
+        create = input("Souhaitez-vous créer un autre utilisateur ? [y] [n] :")
+        if create == "y":
+            create_user(ville)
+        else:
+            return("ok")
+    return("ok")
