45a61268b6b55172769b6450ae23adc32c90d235
Mehdiomalk <83371659+Mehdiomalk@users.noreply.github.com>
2021-07-26 22:14:49 +0200

    1st version script d'authentification

---
+++ Authentification (1).py
@@ -0,0 +1,46 @@
+# script mot_de_passe.py
+import csv
+from tkinter import *
+from tkinter.messagebox import *  # boîte de dialogue
+
+
+def verification():
+    if mdp.get() == 'python27':
+        # le mot de passe est bon : on affiche une boîte de dialogue puis on ferme la fenêtre
+        showinfo('Résultat', 'Mot de passe correct.\nAu revoir !')
+        Mafenetre.destroy()
+    else:
+        # le mot de passe est incorrect : on affiche une boîte de dialogue
+        showwarning('Résultat', 'Mot de passe incorrect.\nVeuillez recommencer !')
+        mdp.set('')
+
+
+# Création de la fenêtre principale (main window)
+Mafenetre = Tk()
+Mafenetre.title('Identification requise')
+
+# Création d'un widget Label (texte 'Login')
+Label2 = Label(Mafenetre, text='Login ')
+Label2.pack(side=LEFT, padx=5, pady=5)
+
+# Création d'un widget Entry (champ de saisie)
+login = StringVar()
+Champ = Entry(Mafenetre, textvariable=login, show="", bg='bisque', fg='maroon')
+Champ.focus_set()
+Champ.pack(side=LEFT, padx=5, pady=5)
+
+# Création d'un widget Label (texte 'Mot de passe')
+Label1 = Label(Mafenetre, text='Mot de passe ')
+Label1.pack(side=LEFT, padx=5, pady=5)
+
+# Création d'un widget Entry (champ de saisie)
+mdp = StringVar()
+Champ = Entry(Mafenetre, textvariable=mdp, show='*', bg='bisque', fg='maroon')
+Champ.focus_set()
+Champ.pack(side=LEFT, padx=5, pady=5)
+
+# Création d'un widget Button (bouton Valider)
+Button = Button(Mafenetre, text='Valider', command=verification)
+Button.pack(side=LEFT, padx=5, pady=5)
+
+Mafenetre.mainloop()
