cd79d923487b45fd4c4314c53010556a8fd9bd9c
adieng17 <82027963+adieng17@users.noreply.github.com>
2021-07-26 20:06:12 +0200

    Modification suite à une erreur de frappe

--- Boite a outil AS.py
+++ Boite a outil AS.py
@@ -1,121 +1,27 @@
-Liste des messages
-Photo de profil de Sandy JEAN-JACQUES.Message par Sandy JEAN-JACQUES, comporte des pièces jointes.
-Sandy JEAN-JACQUES
-Hier 14:12
-Classeur1.csv
-
-Photo de profil de Sandy JEAN-JACQUES.Message par Sandy JEAN-JACQUES, comporte des pièces jointes.
-Sandy JEAN-JACQUES
-Hier 16:12
-match_.txt
-
-je suis au telephone en meme temps par Sandy JEAN-JACQUES
-Sandy JEAN-JACQUESHier 16:12
-je suis au telephone en meme temps
-
-je fais aussi le script de set match par Sandy JEAN-JACQUES
-Sandy JEAN-JACQUESHier 16:12
-je fais aussi le script de set match
-
-yes tqt par Abdoulaye DIENG
-Abdoulaye DIENG
-Hier 16:12
-yes tqt
-
-ok vsy par Abdoulaye DIENG
-Abdoulaye DIENGHier 16:12
-ok vsy
-
-Photo de profil de Sandy JEAN-JACQUES.Message par Sandy JEAN-JACQUES, comporte des pièces jointes.
-Sandy JEAN-JACQUES
-Hier 16:20
-set match.txt
-
-Appel terminé
-Hier 16:51 Appel terminé 2 h 53 min 4 s
-Appel démarré
-Hier 16:51 Appel démarré
-Photo de profil de Mehdi OMALK.Image par Mehdi OMALK
-Mehdi OMALK
-Hier 17:22
-Image
-Photo de profil de Mehdi OMALK. def slave1():     fenetre = Toplevel(C:\... par Mehdi OMALK
-Mehdi OMALK
-Hier 17:31
- def slave1():
-    fenetre = Toplevel(C:\Users\sebastien.OE\Desktop\Programmation\Int_KCAH\Int_Slave1.py)
-    fenetre.mainloop()
-Appel terminé
-Hier 18:10 Appel terminé 1 h 19 min 22 s
-Aujourd'hui
-Photo de profil de Sandy JEAN-JACQUES.Image par Sandy JEAN-JACQUES
-Sandy JEAN-JACQUES
-09:54
-Image
-ok carré par Abdoulaye DIENG
-Abdoulaye DIENG
-10:58
-ok carré
-
-Appel démarré
-19:50 Appel démarré
-Dernière lecture
-Photo de profil de Sandy JEAN-JACQUES.Message par Sandy JEAN-JACQUES, comporte des pièces jointes.
-Sandy JEAN-JACQUES
-19:59
-brute_force.py
-
-Message par Sandy JEAN-JACQUES, comporte des pièces jointes.
-Sandy JEAN-JACQUES19:59
-Boite a outil AS.py
-
-def boite_a_outils(): print("1- Scan de p... par Sandy JEAN-JACQUES
-Sandy JEAN-JACQUES20:03
 def boite_a_outils():
-
 print("1- Scan de ports")
 print("2- Simulation force brute")
 print("3- Quitter")
 
-
-
 choice = input("Ques ouhaites-vous faire ? ")
-
-
 
 if int(choice) == 1:
 
-
-
 scan_port()
 
-
-
 boite_a_outils()
-
-
 
 elif int(choice) == 2:
 
-
-
 brute_force()
 
-
-
 boite_a_outils()
-
-
 
 elif int(choice) == 3:
 
 return("done")
 
-
-
 else:
-
-
 
 print("Merci de rentrer une valeur valide \n")
 boite_a_outils()
